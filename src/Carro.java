6e62bd0a3e53622af212b40305c65f9a5a623e87
Julio Dias <julio.dias@trizy.com.br>
2025-09-30 18:21:02 -0300

    05.09

--- src/Carro.java
+++ src/Carro.java
@@ -5,11 +5,14 @@ public class Carro {
 //É uma classe para representar o tipo, não deve ter o método main.
 //Declaração int ano, declaração mais atribuição é int ano = 2025;
 /*Aqui são os atributos da variável de instancia. Também pode ser chamado de membros
-    que podem ser tanto as variaveis como os metodos*/
+    que podem ser tanto as variáveis como os métodos*/
+//Lembrar ao atribuir valores as variáveis devem ser do tipo declarada.
 
     String fabricante;
     String modelo;
     String cor;
     int anoFabricacao;
+    //Composição de objetos, é a relação tem UM.
+    Pessoa proprietario;
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -2,22 +2,37 @@ public class Principal {
 
     public static void main(String[] args){
 
+        //Sempre lembrar que para usar um objeto ele precisa estar instanciado = existir.
+
         //criando um objeto da classe Carro, é um objeto único.
         Carro meuCarro = new Carro();
         meuCarro.fabricante = "Honda";
         meuCarro.modelo = "Civic";
         meuCarro.cor = "Branco";
         meuCarro.anoFabricacao = 2025;
+        //Aqui gera uma exceção nullPointerException, Resolvido: Instanciando uma pessoa, tem que existir a pessoa.
+        meuCarro.proprietario = new Pessoa();
+        meuCarro.proprietario.nome = "Julio";
+        meuCarro.proprietario.cpf = "123.456.789-00";
+        meuCarro.proprietario.anoNascimento = 2003;
 
-        System.out.println("O fabricante do meu carro é " + meuCarro.fabricante);
+        //outra forma de criar esse proprietario, é criando o objeto da pessoa é so atribuir a variavel proprietario.
+        Pessoa julio = new Pessoa();
+        julio.nome = "Julio";
+        julio.cpf = "123.456.789-00";
+        julio.anoNascimento = 2003;
+        meuCarro.proprietario = julio;
 
         //é um objeto diferente do de cima, cada objeto é único, faz referência ao um objeto diferente na memória.
         Carro seuCarro = new Carro();
         seuCarro.fabricante = "Toyota";
         seuCarro.modelo = "Corolla";
-        seuCarro.cor = "Preta";
+        seuCarro.cor = "Branco";
         seuCarro.anoFabricacao = 2025;
-
+        seuCarro.proprietario = new Pessoa();
+        seuCarro.proprietario.nome = "Alex";
+        seuCarro.proprietario.cpf = "123.456.789-00";
+        seuCarro.proprietario.anoNascimento = 1984;
 
     }
 }
