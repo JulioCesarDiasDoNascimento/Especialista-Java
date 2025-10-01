8027e681edd76cf4e9d000be46611dbbc4b8c54e
Julio Dias <julio.dias@trizy.com.br>
2025-09-30 22:35:07 -0300

    05.16

--- src/Carro.java
+++ src/Carro.java
@@ -7,12 +7,32 @@ public class Carro {
 /*Aqui são os atributos da variável de instancia. Também pode ser chamado de membros
     que podem ser tanto as variáveis como os métodos*/
 //Lembrar ao atribuir valores as variáveis devem ser do tipo declarada.
+/* Valores padrões :
+* int = 0
+* String = null (OBS: null é so para objetos)
+* double = 0.0
+* boolean = false */
 
+    //Iniciar as caracteristicas do objeto = atributos.
     String fabricante;
     String modelo;
     String cor;
+    double valor;
     int anoFabricacao;
     //Composição de objetos, é a relação tem UM.
     Pessoa proprietario;
+//Pessoa proprietario = new Pessoa(); sempre instanciar o objeto na classe de declaração é errado.
 
+    //Iniciar os comportamentos do objeto = métodos.
+    void calcularValorRevenda() {
+         int tempoUsoAnos = 2025 - anoFabricacao;
+         int vidaUtilAnos = 20;
+         double valorRevenda = (valor / vidaUtilAnos) * (vidaUtilAnos - tempoUsoAnos);
+
+         if (valorRevenda < 0) {
+             valorRevenda = 0;
+         }
+         System.out.println("Tempo de uso: " + tempoUsoAnos);
+         System.out.println("Valor da revenda: " + valorRevenda);
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -9,21 +9,26 @@ public class Principal {
         meuCarro.fabricante = "Honda";
         meuCarro.modelo = "Civic";
         meuCarro.cor = "Branco";
-        meuCarro.anoFabricacao = 2025;
+        meuCarro.anoFabricacao = 2015;
+        meuCarro.valor = 120000.00;
         //Aqui gera uma exceção nullPointerException, Resolvido: Instanciando uma pessoa, tem que existir a pessoa.
         meuCarro.proprietario = new Pessoa();
         meuCarro.proprietario.nome = "Julio";
-        meuCarro.proprietario.cpf = "123.456.789-00";
+        meuCarro.proprietario.cpf = "123.456.789-08";
         meuCarro.proprietario.anoNascimento = 2003;
 
-        //outra forma de criar esse proprietario, é criando o objeto da pessoa é so atribuir a variavel proprietario.
+        //Outra forma de criar esse proprietario, é criando o objeto da pessoa é so atribuir a variavel proprietario.
+        //As vezes é melhor instanciar de forma separada e depois atribuir.
         Pessoa julio = new Pessoa();
         julio.nome = "Julio";
         julio.cpf = "123.456.789-00";
         julio.anoNascimento = 2003;
         meuCarro.proprietario = julio;
 
-        //é um objeto diferente do de cima, cada objeto é único, faz referência ao um objeto diferente na memória.
+        //Isso é uma pilha de métodos, o main chama o calcularValorRevenda.
+        meuCarro.calcularValorRevenda();
+
+        //É um objeto diferente do de cima, cada objeto é único, faz referência ao um objeto diferente na memória.
         Carro seuCarro = new Carro();
         seuCarro.fabricante = "Toyota";
         seuCarro.modelo = "Corolla";
@@ -33,6 +38,11 @@ public class Principal {
         seuCarro.proprietario.nome = "Alex";
         seuCarro.proprietario.cpf = "123.456.789-00";
         seuCarro.proprietario.anoNascimento = 1984;
+        seuCarro.calcularValorRevenda(); //chamada do método calcularValorRevenda.
+
+        /* Variável: meuCarro, seuCarro e julio, ela não representa o objeto e sim faz referência ao objeto,
+        * então ela não armazena valor.
+        * O objeto é único*/
 
     }
 }
