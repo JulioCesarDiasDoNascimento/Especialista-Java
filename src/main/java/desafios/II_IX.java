dc724751bd72b4bb881ca6033e53a19c5f2c146e
Julio Cesar <juliocesarddn@gmail.com>
2026-05-21 15:22:54 -0300

    Desafio 02.09 Variáveis e operadores aritméticos

---
+++ src/main/java/desafios/II_IX.java
@@ -0,0 +1,11 @@
+package desafios;
+
+public class II_IX {
+    public static void main(String[] args) {
+        int vendaJoao = 20;
+        int vendaGiovanna = 30;
+        int vendaJulio = 100;
+        int mediaVendas = (vendaJulio + vendaGiovanna + vendaJoao) / 3;
+        System.out.println("O valor medio é: " +  mediaVendas);
+    }
+}
