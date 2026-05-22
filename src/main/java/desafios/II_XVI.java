12817ab20fbe0a5f4883f54ab9183ee761459bf9
Julio Cesar <juliocesarddn@gmail.com>
2026-05-22 04:16:40 -0300

    Desafio 02.16 Tipos primitivos e conversão

---
+++ src/main/java/desafios/II_XVI.java
@@ -0,0 +1,14 @@
+package desafios;
+
+public class II_XVI {
+    public static void main(String[] args) {
+        double pesoCarga1 = 1033.49;
+        double pesoCarga2 = 849.88;
+
+        double somaCarga = pesoCarga1 + pesoCarga2;
+        int pesoTotalCargas = (int)somaCarga;
+
+        System.out.println("Peso Total: " + pesoTotalCargas);
+
+    }
+}
